91df250fa87165daa52dd6e7e659c07711a68311
Destin <d.gandobak@gmail.com>
2021-06-10 10:23:38 +0200

    Create your own dotenv to loads environment variables from .env file

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . '/SimplyDotEnv.php';
+// use Dotenv\SimplyDotEnv;     (if you use composer or auto-loading)
+
+(new Dotenv\SimplyDotEnv(__DIR__))->load();
+
+echo getenv('APP_ENV');
+echo "\n";
+echo getenv('DATABASE_DNS');
